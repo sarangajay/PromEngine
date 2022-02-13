65ad3ab4c3b7b293e9e8b65eff52a87b756c40be
sarangajay <Oneli2008>
2022-02-13 17:03:10 +0100

    PromoEngineService initialization enhanced. lombok is added to the project Item data transfer object added

--- src/main/java/com/sj/promotion/PromoEngineService.java
+++ src/main/java/com/sj/promotion/PromoEngineService.java
@@ -1,8 +1,17 @@
 package com.sj.promotion;
 
+import com.sj.promotion.dto.Item;
+
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class PromoEngineService {
+
+    private final List<Item> items;
+
+    public PromoEngineService(List<Item> items) {
+        this.items = items;
+    }
 
 
     public Double doCalculate() {
--- src/test/java/com/sj/promotion/PromoEngineServiceTest.java
+++ src/test/java/com/sj/promotion/PromoEngineServiceTest.java
@@ -1,7 +1,10 @@
 package com.sj.promotion;
 
+import com.sj.promotion.dto.Item;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,8 +14,12 @@ public class PromoEngineServiceTest {
     @DisplayName("Test PromoEngineService.doCalculate()")
     @Test
     void empty_shopping_cart() {
-        PromoEngineService promoEngineService = new PromoEngineService();
+        PromoEngineService promoEngineService = buildCartWithItems();
         assertEquals(5.00, promoEngineService.doCalculate());
+    }
+
+    private PromoEngineService buildCartWithItems(Item... items) {
+        return new PromoEngineService(Arrays.asList(items));
     }
 
 
