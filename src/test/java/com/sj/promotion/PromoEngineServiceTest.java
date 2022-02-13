bd0ea62306a5bbdc42b1eebafc6c5e45be27c52b
sarangajay <Oneli2008>
2022-02-13 17:45:26 +0100

    total_of_single_shopping_cart failed test

--- src/test/java/com/sj/promotion/PromoEngineServiceTest.java
+++ src/test/java/com/sj/promotion/PromoEngineServiceTest.java
@@ -18,6 +18,13 @@ public class PromoEngineServiceTest {
         assertEquals(5.00, promoEngineService.doCalculate());
     }
 
+    @DisplayName("Test PromoEngineService.doCalculate() with single shopping cart")
+    @Test
+    public void total_of_single_shopping_cart() {
+        PromoEngineService promoEngineService = buildCartWithItems(new Item("A", 2));
+        assertEquals(100.0, promoEngineService.doCalculate());
+    }
+
     private PromoEngineService buildCartWithItems(Item... items) {
         return new PromoEngineService(Arrays.asList(items));
     }
