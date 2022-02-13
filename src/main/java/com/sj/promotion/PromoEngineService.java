89212abd596d6970e253c10385a030d514bfe20a
sarangajay <Oneli2008>
2022-02-13 16:42:06 +0100

    empty_shopping_cart failed test

--- src/main/java/com/sj/promotion/PromoEngineService.java
+++ src/main/java/com/sj/promotion/PromoEngineService.java
@@ -1,4 +1,14 @@
 package com.sj.promotion;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class PromoEngineService {
+
+
+    public Double doCalculate() {
+        AtomicReference<Double> total = new AtomicReference<>(0.00);
+
+        return total.get();
+
+    }
 }
--- src/test/java/com/sj/promotion/PromoEngineServiceTest.java
+++ src/test/java/com/sj/promotion/PromoEngineServiceTest.java
@@ -1,4 +1,19 @@
 package com.sj.promotion;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class PromoEngineServiceTest {
+
+    @DisplayName("Test PromoEngineService.doCalculate()")
+    @Test
+    void empty_shopping_cart() {
+        PromoEngineService promoEngineService = new PromoEngineService();
+        assertEquals(5.00, promoEngineService.doCalculate());
+    }
+
+
 }
